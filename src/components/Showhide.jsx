8e8949ebbcbeee409b6bb8609819d2dab7c2d820
Anagha402 <anagharamesh18@gmail.com>
2024-10-24 11:25:31 +0530

    second

--- src/components/Showhide.jsx
+++ src/components/Showhide.jsx
@@ -4,25 +4,25 @@ import { MDBBtn } from 'mdb-react-ui-kit';
 function Showhide() {
   
   // useState hook to manage visibility state
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState();
 
   // Show function sets visibility to true
   function show() {
-    setIsVisible(true);
+    setIsVisible("Hello");
   }
 
   // Hide function sets visibility to false
   function hide() {
-    setIsVisible(false);
+    setIsVisible();
 
   }
   return (
     <div className='container text-center border border-success mt-5 p-5'>
       <div className="container bg-light  p-5">
-        <img className='m-5' src="https://static.vecteezy.com/system/resources/previews/014/440/980/original/email-message-icon-design-in-blue-circle-png.png" height={150} />
-  {/* Conditionally render the <p> element based on isVisible */}
+        <img className='m-5' src="https://static.vecteezy.com/system/resources/previews/014/440/980/original/email-message-icon-design-in-blue-circle-png.png" height={150} alt="" />
+
         <div id="display"className='bg-light' >
-        {isVisible && <p className='bg-light'style={{fontWeight:600,fontSize:20}}>Hello</p>}
+        <p className='bg-light'style={{fontWeight:600,fontSize:20}}>{isVisible}</p>
         
         </div>
 <br /><br />
